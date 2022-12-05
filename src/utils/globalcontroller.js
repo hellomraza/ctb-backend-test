d042217ca7361451aa8895055b08da80e51d6fc6
rizwan92 <chouhan.rizwan@gmail.com>
2022-12-05 15:20:47 +0530

    JWT_SECRET

--- src/utils/globalcontroller.js
+++ src/utils/globalcontroller.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const GlobalController = {
   createToken: ({ email }) => {
-    return jwt.sign({ sub: email }, process.env.SECRET);
+    return jwt.sign({ sub: email }, process.env.JWT_SECRET);
   },
 };
 
